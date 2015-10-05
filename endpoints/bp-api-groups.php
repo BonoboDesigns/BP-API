1df107fa1356285783904c91e3e6d8629c4e4c01
abraham <abe@unscene.us>
2015-10-05 04:31:22 -0700

    -one way to achieve single group pages for json api is to loop through posts, then find post with same slug attached, then if it's true, push to  array, if false, skip to next item

--- endpoints/bp-api-groups.php
+++ endpoints/bp-api-groups.php
@@ -53,9 +53,9 @@ class BP_API_Groups extends WP_REST_Controller {
 	 * @param mixed $request
 	 * @return void
 	 */
-	public function get_item( $request ) {
+	public function get_item( $filter = array() ) {
 
-		$response = 'a single group item';
+		$response = $this->get_single_group( $filter['filter'] );
 
 		return $response;
 
@@ -80,7 +80,7 @@ class BP_API_Groups extends WP_REST_Controller {
 
 				bp_the_group();
 					ob_start();
-					bp_get_group_avatar('html=false&url=true&type=full');
+					bp_group_avatar('html=false&url=true&type=full');
 					$groAvatar = ob_get_clean();
 					$groDoc = new DOMDocument();
 					$groDoc->loadHTML($groAvatar);
@@ -95,13 +95,14 @@ class BP_API_Groups extends WP_REST_Controller {
 						$groAvatar = $groAvatar;
 					} 
 				$group = array(
-					'abe_desc' => bp_get_group_description_excerpt(),
+					'excerpt_last' => bp_get_group_description_excerpt(),
 					'abe_last' => bp_get_group_last_active(),
 					'abe_permalink' => bp_get_group_permalink(),
 					'abe_type' => bp_get_group_type(),
 					'excerpt' => bp_get_group_member_count(),
 					'featured_image' => $groAvatar,
 					'ID' => bp_get_group_id(),
+					'slug' => bp_get_group_slug(),
 					'title' => bp_get_group_name()
 				);
 
@@ -113,6 +114,77 @@ class BP_API_Groups extends WP_REST_Controller {
 
 			$data = array(
 				'groups' => $groupees
+			);
+
+			$data = apply_filters( 'bp_json_prepare_groupees', $data );
+
+		} else {
+			return new WP_Error( 'bp_json_group', __( 'No group Found.', 'buddypress' ), array( 'status' => 200 ) );
+		}
+
+		$response = new WP_REST_Response();
+		$response->set_data( $data );
+		$response = rest_ensure_response( $response );
+
+		return $response;
+
+	}
+
+	public function get_single_group( $filter ) {
+
+		// $sing_group = groups_get_group( array( 'group_id' => $id ) );
+		// $slug = $sing_group->slug;
+
+		$args = $filter;
+		// $args = array(
+					// 'slug' => $filter['slug'],
+					// 'max' => 1
+		// );
+		// $args = $groupPush;
+
+		if ( bp_has_groups( $args ) ) {
+
+			while ( bp_groups() ) {
+
+				bp_the_group();
+					ob_start();
+					bp_group_avatar('html=false&url=true&type=full');
+					$groAvatar = ob_get_clean();
+					$groDoc = new DOMDocument();
+					$groDoc->loadHTML($groAvatar);
+					$groImageTags = $groDoc->getElementsByTagName('img');
+				    foreach($groImageTags as $grotag) {
+				        $groParsedAvatar = $grotag->getAttribute('src');
+				    }
+
+					if ($groParsedAvatar != null) {
+						$groAvatar = $groParsedAvatar;
+					} else {
+						$groAvatar = $groAvatar;
+					}
+					if (bp_get_group_slug() == $filter['slug']) {
+						$group = array(
+							'excerpt_last' => bp_get_group_description_excerpt(),
+							'abe_last' => bp_get_group_last_active(),
+							'abe_permalink' => bp_get_group_permalink(),
+							'abe_type' => bp_get_group_type(),
+							'excerpt' => bp_get_group_member_count(),
+							'featured_image' => $groAvatar,
+							'ID' => bp_get_group_id(),
+							'slug' => bp_get_group_slug(),
+							'title' => bp_get_group_name()
+						);
+					} else {}
+				
+
+				$group = apply_filters( 'bp_json_prepare_group', $group );
+
+				// $groupees[] = $group;
+
+			}
+
+			$data = array(
+				'group' => $group
 			);
 
 			$data = apply_filters( 'bp_json_prepare_groupees', $data );
